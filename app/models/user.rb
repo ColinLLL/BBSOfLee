3f55f4e012560a21820d865456dcc9487717ab22
Colin Lee <colin@originatechina.com>
2013-12-16 17:48:58 +0800

    Homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
-  validates :email, presence: true, uniqueness: true, format: { 
+  validates :email, presence: true, format: { 
     with: /(\w*)@(\w*).com/,
     message: 'must be a Email format'}
 end
