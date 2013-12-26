d4dc5b264f4b28342b76f59f6536a47a6d92b4eb
ColinLLL <colin@originatechina.com>
2013-12-26 15:58:37 +0800

    BBSOfLee

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "lilonglongfwz@163.com"
+  
   def registration_confirmation(user)
-  	#recipients = user.email
   	@user = user
-  	#@url = 'http://www.126.com'
   	mail(to: @user.email, subject: "Thank you for Registstering")
-  	#from:       "lilonglongfwz@163.com"
-  	#subject    = "Thank you for Registstering"
-  	#body       = {:user => user}
-  	#to:         "lilonglongfwz@163.com"
-  	#from:       "lilonglongfwz@163.com"
-  	#reply_to:   "lilonglongfwz@163.com"
   end
 end
