6fc53237303578b3094a1f26f332198825427aea
ColinLLL <colin@originatechina.com>
2013-12-30 11:24:29 +0800

    BBSOfLee

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   validates :password, presence: true, length: { minimum: 6 }, :confirmation => true
-  validates :email, presence: true, format: { with: /(\w*)@(\w*).com/, message: 'must be a Email format'}
+  validates :email, presence: true, format: { with: /(\w*)@(\w*).com/, message: 'must be a Email format'}, uniqueness: true
 
   has_secure_password
 
