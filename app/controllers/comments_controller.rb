fddec4a645128afe3d83d08d17ac03a4399c017c
ColinLLL <colin@originatechina.com>
2014-01-09 10:09:08 +0800

    BBSOfLee

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,9 @@
 class CommentsController < ApplicationController
   def create
   	@post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
+  	user_id = session[:user_id]
+    @comment = @post.comments.create(params[:comment].permit(:body))
+    @comment.user_id = user_id
     redirect_to post_path(@post)
   end
 end
