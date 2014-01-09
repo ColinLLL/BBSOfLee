a7717e3dd9d93039f30c289b630b0509e9779bd2
ColinLLL <colin@originatechina.com>
2014-01-09 10:30:37 +0800

    BBSOfLee

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
   def create
+  	@current_user = User.find_by_id(session[:user_id])
   	@post = Post.find(params[:post_id])
-  	user_id = session[:user_id]
+  	#user_id = session[:user_id]
     @comment = @post.comments.create(params[:comment].permit(:body))
-    @comment.user_id = user_id
+    @comment.user_id = @current_user.id
+    @comment.save
     redirect_to post_path(@post)
   end
 end
