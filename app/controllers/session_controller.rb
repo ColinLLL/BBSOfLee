db742e240e310fc5d7ad004ec5ff3f975bd65c5f
ColinLLL <colin@originatechina.com>
2014-01-14 14:36:49 +0800

    BBSOfLee

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,16 +5,7 @@ class SessionController < ApplicationController
 
 
   def forgetpassword
-    if user = User.find_by_email(params[:email])
-      newpassword = (('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a).shuffle[0..6].join
-      user.password = newpassword
 
-      UserMailer.registration_confirmation(user).deliver
-
-      redirect_to login_path, alert: "The new password has been sent to your register email."
-    else
-      redirect_to forgetpassword_path, alert: "The email address can't be found"
-    end
   end
 
   def create
